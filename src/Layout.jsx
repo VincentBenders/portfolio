18dc06b4ff65177402c721dddeb90fe51218c305
vincent benders <vincentbenders@gmail.com>
2025-04-06 21:41:13 +0200

    fix git ignore & nav padding

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -5,7 +5,7 @@ export default class Layout extends Component {
   render() {
     return (
         <>
-        <nav className={'border-black'}>
+         <nav className={'bg-white space-x-2 border-gray-200 dark:bg-gray-900 dark:border-gray-700 p-5'}>
             <Link className={'bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded'} to={"/"}>Portfolio</Link>
             <Link className={'bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded'} to={"/about"}>About Me</Link>
            
